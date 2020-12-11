8182cf1e751aaa37b636689516e0cc9daa1f1476
koalatron-9000 <piratesballkoala@gmail.com>
2020-12-10 17:30:16 -0800

    Implemented pillow function to resize and rotate images

--- Week1/image_resizer.py
+++ Week1/image_resizer.py
@@ -1,6 +1,12 @@
 from PIL import Image
 import glob
+import os
 
 GL = glob.glob("/home/koalatron9000/Pictures/*.png")
-for file in GL:
-    print(file)
+NewD = "/reworked/"
+for each in GL:
+    print(each)
+    s=os.path.split(each)
+    img = Image.open(each).rotate(270).resize((128, 128))
+    img.save(s[0]+NewD+s[1])
+
