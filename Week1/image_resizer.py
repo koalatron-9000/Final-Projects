26ee560f5d89d1674d989c4b959799ab7b013693
koalatron-9000 <piratesballkoala@gmail.com>
2020-12-11 10:05:06 -0800

    Removed personal data and added notation

--- Week1/image_resizer.py
+++ Week1/image_resizer.py
@@ -1,12 +1,14 @@
 from PIL import Image
 import glob
 import os
+'''This script will batch process a folder of pictures and save them in a seperate folder.'''
 
-GL = glob.glob("/home/koalatron9000/Pictures/*.png")
-NewD = "/reworked/"
-for each in GL:
-    print(each)
-    s=os.path.split(each)
-    img = Image.open(each).rotate(270).resize((128, 128))
-    img.save(s[0]+NewD+s[1])
+
+GL = glob.glob("/insert/original/filepath/here/") #Glob searches a filepath and returns a list
+NewD = "/instert/new/directory/here/" #Defines a target file path
+
+for each in GL: #Iterates through the each item of the GL list
+    s=os.path.split(each) #splits the the file name from the file path
+    img = Image.open(each).rotate(270).resize((128, 128)) #Opens a file(each) and does operations
+    img.save(NewD+s[1]) #Saves processed file in the new directory 
 
