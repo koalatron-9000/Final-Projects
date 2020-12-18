6684995b072f47203de6a794fff732ce7874ca04
koalatron-9000 <piratesballkoala@gmail.com>
2020-12-18 14:44:55 -0800

    Fleshed out changeImage.py and got it working.

--- Week4/changeImage.py
+++ Week4/changeImage.py
@@ -1,7 +1,15 @@
-import PIL
+from PIL import Image
+import glob
 
-#change image from 3000X2000 to 600X400. 
-# X	and change image format from .TIFF to .JPEG.
-# X	convert from RGBA to RGB.
-# X	images will be in "/supplier-data/images/".
-# X	import PIL X
+'''This script will batch process a folder of pictures and save them in a seperate folder.'''
+
+GL = glob.glob("/home/koalatron9000/Pictures/*.png") #Glob searches a filepath and returns a list
+
+for each in GL: #Iterates through the each item of the GL list
+    img = Image.open(each).resize((600, 400)) #Opens a file(each) and does operations
+    img = img.convert("RGB") #Converts the RGBA files to RGB
+    name = each.split(".") #Splits the file name from the file extention
+    file_extension = ".jpeg" #New file extension
+    new_name = str(name[0]) +file_extension #Combines the file name with the new extension
+    img.save(new_name, "JPEG") #Saves processed file 
+    
