2400c45160cbbe234a2ad3fc36f3ca5f14bdeab3
koalatron-9000 <piratesballkoala@gmail.com>
2020-12-18 16:48:45 -0800

    fleshed out run.py and it now works. Updated problem_statement.txt to reflect this

--- Week4/run.py
+++ Week4/run.py
@@ -7,3 +7,15 @@ import requests
 #	url will be http://[linux-instance-external-IP]/fruits
 #	import os X
 #	import requests X
+files = "/home/koalatron9000/test"
+reviews = os.listdir(files)
+
+for each in reviews:
+    with open(each, "r") as f:
+        ReadData = list(f)
+        payload = {"name":str(ReadData[0]).replace("\n",""),
+        "weight":str(ReadData[1][:-4]).replace("\n",""),
+        "description":str(ReadData[2:]).replace("\n","")}
+        r = requests.post("http://[linux-instance-external-IP]/fruits", data=payload)
+        print(r)
+        
