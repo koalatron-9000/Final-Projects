b87b2edef0e42841baa531caa4e5cbb99ab9ba77
koalatron-9000 <piratesballkoala@gmail.com>
2020-12-18 16:00:15 -0800

    Fleshed out the Supplier_image_upload.py and it SHOULD work. can't test until on the server. I will revise with any fixes required.

--- Week4/supplier_image_upload.py
+++ Week4/supplier_image_upload.py
@@ -1,6 +1,13 @@
 import requests
+import glob
 
 #upload images from "/supplier-data/images/" to the website.
 #	use example_upload.py as a base for this file.
 #	url will be [linux-instance-IP-Address]/upload
 #	import requests X
+url = "[linux-instance-IP-Address]/upload/"
+GL = glob.glob("/home/koalatron9000/Pictures/*.jpeg")
+for each in GL:
+    with open(each,"rb") as opened:
+        requests.post(url, files ={"file": opened})
+         
